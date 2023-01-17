23b3cc6bf3112aafb2f84d569ab9ea23df575923
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-17 21:07:42 +0530

    menu list component code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+import { MenuList } from "./components/MenuList/MenuList";
 import { MenuListItem } from "./components/MenuListItem/MenuListItem";
+import s from "./style.module.css";
+
 export function App() {
   return (
     <div>
-      <MenuListItem difficulty="Low" />
-      <DisplayDifficulty difficulty="Low" />
+      <h1>Select your React difficulty level!</h1>
+      <div className={s.workspace}>
+        <MenuList />
+        {/* <MenuListItem difficulty="Low" /> */}
+        <DisplayDifficulty difficulty="Low" />
+      </div>
     </div>
   );
 }
--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import s from "./style.module.css"
+import s from "./style.module.css";
 export const MenuListItem = (props) => {
   const [isHovered, setIsHover] = useState(false);
   function activate() {
@@ -17,7 +17,8 @@ export const MenuListItem = (props) => {
     }
   }
   return (
-    <div className={s.container}
+    <div
+      className={s.container}
       onMouseEnter={activate}
       onMouseLeave={deactivate}
       style={{ backgroundColor: getBGC() }}
