233cfc58284b4dbe920085be39026cfb43fcbc19
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-17 20:50:05 +0530

    menuListItem code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
-
+import { MenuListItem } from "./components/MenuListItem/MenuListItem";
 export function App() {
   return (
     <div>
+      <MenuListItem difficulty="Low" />
       <DisplayDifficulty difficulty="Low" />
     </div>
   );
